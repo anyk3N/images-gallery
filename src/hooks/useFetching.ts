65076e60d3e8c7a1fff34965e703d36f53423d44
anyk3N <vuk0lov@yandex.by>
2025-09-07 22:43:19 +0300

    Adding hooks and svg handler

---
+++ src/hooks/useFetching.ts
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export const useFetching = <T extends (...args: any[]) => Promise<any>>(
+    callback: T
+) => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
+
+    const fetching = async (...args: Parameters<T>): Promise<ReturnType<T> | void> => {
+        try {
+            setIsLoading(true);
+            setError('');
+            return await callback(...args);
+        } catch (error) {
+            setError(error instanceof Error ? error.message : 'Unknown error');
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return [fetching, isLoading, error] as const;
+}
